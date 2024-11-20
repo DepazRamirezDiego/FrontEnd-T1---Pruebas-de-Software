39528e0a9a8503c3464d059b97c54c9ff7f39480
LorenaBenites <130251425+LorenaBenites@users.noreply.github.com>
2024-11-19 23:30:12 -0500

    se agrego el formulario y mejoro diseno

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PaginaInicioComponent } from './components/pagina-inicio/pagina-inicio.component';
+import { RegistroDeEquiposComponent } from './components/registro-de-equipos/registro-de-equipos.component';
 
 const routes: Routes = [
   { path: '', component: PaginaInicioComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' }
-
+  { path: 'registro', component: RegistroDeEquiposComponent },
+  
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
